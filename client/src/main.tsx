3b8ad94e353a2a0264e5f5da2b0bec33851d6d38
JM <Not given>
2025-05-28 15:12:37 -0400

    Remove unused code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ReactDOM from "react-dom/client";
@@ -10,12 +8,6 @@ const router = createBrowserRouter([
     { path: "/", Component: App },
     { path: "/test", element: <div>Test path</div> }
 ])
-
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-)
 
 const root = document.getElementById("root")!;
 

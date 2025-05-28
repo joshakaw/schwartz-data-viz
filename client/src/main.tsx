53ec3aaf20e6123f8098146b7a3968fd0a102733
JM <Not given>
2025-05-28 15:00:02 -0400

    Added DOM routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+
+const router = createBrowserRouter([
+    { path: "/", Component: App },
+    { path: "/test", element: <div>Test path</div> }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
+
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
+    <RouterProvider router={router} />
 )

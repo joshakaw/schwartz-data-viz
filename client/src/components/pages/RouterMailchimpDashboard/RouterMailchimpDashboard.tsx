6e61f804ff7576bf1e36ebb3dc97c501d2e3f52f
JM <Not given>
2025-05-29 23:06:55 -0400

    Transferred content to respective page components

--- client/src/components/pages/RouterMailchimpDashboard/RouterMailchimpDashboard.tsx
+++ client/src/components/pages/RouterMailchimpDashboard/RouterMailchimpDashboard.tsx
@@ -3,9 +3,15 @@ import './RouterMailchimpDashboard.css';
 
 interface RouterMailchimpDashboardProps {}
 
+/**
+ * MailchimpDashboard Component
+ * Displays a dashboard interface for Mailchimp integration.
+ */
 const RouterMailchimpDashboard: FC<RouterMailchimpDashboardProps> = () => (
   <div className="RouterMailchimpDashboard">
-    RouterMailchimpDashboard Component
+        <h1>Mailchimp Dashboard</h1>
+        {/* TODO: Add dashboard widgets and Mailchimp integration here */}
+        <p>Welcome to your Mailchimp Dashboard. Here you can view campaign stats, manage subscribers, and more.</p>
   </div>
 );
 
--- client/src/components/pages/RouterSignupsDashboard/RouterSignupsDashboard.tsx
+++ client/src/components/pages/RouterSignupsDashboard/RouterSignupsDashboard.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import './RouterSignupsDashboard.css';
+import BarChart from '../../bar-chart/bar-chart';
 
 interface RouterSignupsDashboardProps {}
 
 const RouterSignupsDashboard: FC<RouterSignupsDashboardProps> = () => (
   <div className="RouterSignupsDashboard">
-    RouterSignupsDashboard Component
+        RouterSignupsDashboard Component
+        <BarChart />
   </div>
 );
 

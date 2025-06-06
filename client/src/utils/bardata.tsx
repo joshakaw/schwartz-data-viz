86c13c4ed544c50f1277ca2739b1f314aa4988f7
rehtyric <salispahict@gmail.com>
2025-06-05 21:11:41 -0400

    Finished bootstrap styling sortof

--- client/src/utils/bardata.tsx
+++ client/src/utils/bardata.tsx
@@ -6,58 +6,21 @@
 // BarChart.tsx will not change. only thing changing is the data on it. which is currently supplied here
 
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Select from 'react-select';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/style.css';
+import { Overlay } from 'react-bootstrap';
 
 export interface SignupData {
     category: string;
     signups: number;
 }
-//export interface Signup {
-//    category: string;
-//    userType: string;
-//    date: Date;
-//}
-
-// Example data until we have a server connection
-//export const signups: Signup[] = [
-//    { category: "Social Media", date: new Date("2025-04-03"), userType: "Tutor" },
-//    { category: "Email Campaign", date: new Date("2025-04-07"), userType: "Parent" },
-//    { category: "Physical Advertising", date: new Date("2025-04-12"), userType: "Student" },
-//    { category: "Friend Referral", date: new Date("2025-04-15"), userType: "Tutor" },
-//    { category: "Social Media", date: new Date("2025-04-20"), userType: "Student" },
-//    { category: "Email Campaign", date: new Date("2025-04-25"), userType: "Parent" },
-//    { category: "Physical Advertising", date: new Date("2025-04-29"), userType: "Tutor" },
-//    { category: "Friend Referral", date: new Date("2025-05-01"), userType: "Student" },
-//    { category: "Social Media", date: new Date("2025-05-05"), userType: "Parent" },
-//    { category: "Email Campaign", date: new Date("2025-05-09"), userType: "Tutor" },
-//    { category: "Physical Advertising", date: new Date("2025-05-12"), userType: "Student" },
-//    { category: "Friend Referral", date: new Date("2025-05-17"), userType: "Parent" },
-//    { category: "Social Media", date: new Date("2025-05-20"), userType: "Tutor" },
-//    { category: "Email Campaign", date: new Date("2025-05-23"), userType: "Student" },
-//    { category: "Physical Advertising", date: new Date("2025-05-27"), userType: "Parent" },
-//    { category: "Friend Referral", date: new Date("2025-05-30"), userType: "Tutor" },
-//    { category: "Social Media", date: new Date("2025-04-08"), userType: "Student" },
-//    { category: "Email Campaign", date: new Date("2025-04-14"), userType: "Parent" },
-//    { category: "Physical Advertising", date: new Date("2025-04-19"), userType: "Tutor" },
-//    { category: "Friend Referral", date: new Date("2025-04-24"), userType: "Student" },
-//    { category: "Social Media", date: new Date("2025-05-03"), userType: "Parent" },
-//    { category: "Email Campaign", date: new Date("2025-05-08"), userType: "Tutor" },
-//    { category: "Physical Advertising", date: new Date("2025-05-14"), userType: "Student" },
-//    { category: "Friend Referral", date: new Date("2025-05-18"), userType: "Parent" },
-//    { category: "Social Media", date: new Date("2025-05-25"), userType: "Tutor" },
-//    { category: "Email Campaign", date: new Date("2025-05-28"), userType: "Student" },
-//    { category: "Physical Advertising", date: new Date("2025-04-05"), userType: "Parent" },
-//    { category: "Friend Referral", date: new Date("2025-04-10"), userType: "Tutor" },
-//    { category: "Social Media", date: new Date("2025-04-16"), userType: "Student" },
-//    { category: "Email Campaign", date: new Date("2025-04-21"), userType: "Parent" }
-//];
 
 
 // Example raw data.
@@ -68,7 +31,23 @@ export const rawSignupData: SignupData[] = [
     { category: 'Email Campaign', signups: 12 }
 ];
 
-// Hi tarik
+const Signup = [
+    { value: 'Social Media', label: 'Social Media' },
+    { value: 'Physical Advertising', label: 'Physical Advertising' },
+    { value: 'Friend Referral', label: 'Friend Referral' },
+    { value: 'Email Campaign', label: 'Email Campaign' },
+];
+
+const User = [
+    { value: 'Students', label: 'Students' },
+    { value: 'Parents', label: 'Parents' },
+    { value: 'Tutors', label: 'Tutors' }
+];
+
+const School = [
+    { value: 'K-12', label: 'K-12' },
+    { value: 'University', label: 'University' },
+];
 
 const Options = () => {
     const [timeframe, setTimeframe] = useState<Date[] | undefined>();
@@ -76,13 +55,9 @@ const Options = () => {
     const [schoolType, setSchoolType] = useState("default");
     const [signupType, setSignupType] = useState("default");
 
-    // const [rawSignupData, setRawSignupData] = useState<SignupData[]>([]);
+    const target = useRef(null);
 
-    const TimeframeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setTimeframe([new Date(event.target.value)]);
-        // Given how this uses a date range, may need special parsing before SQL call.
-        // SQL Call with new timeframe and curr usertype
-    };
+    // const [rawSignupData, setRawSignupData] = useState<SignupData[]>([]);
 
     const UserTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setUserType(event.target.value);
@@ -90,11 +65,13 @@ const Options = () => {
     };
 
     const SchoolChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        console.log(schoolType);
         setSchoolType(event.target.value);
         // SQL Call with new usertype and curr usertype
     };
 
     const SignupChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        alert(signupType);
         setSignupType(event.target.value);
         // SQL Call with new usertype and curr usertype
     };
@@ -130,41 +107,59 @@ const Options = () => {
         <Container fluid>
             <h1>Filters</h1>
             <Row>
-                <Form>
-                    <Form.Group controlId="filters">
-                        <Col>
-                            <Form.Label>Signup Method:</Form.Label>
-                            <Form.Select value="signupMethod" onChange={SignupChange}>
-                                <option value="default">All methods</option>
-                                <option value="default">Social Media</option>
-                                <option value="default">Physical Advertising</option>
-                                <option value="default">Friend Referral</option>
-                                <option value="default">Email Campaign</option>
-                            </Form.Select>
-                        </Col>
-                        <Col>
-                            <Form.Label>Account Type:</Form.Label>
-                            <Form.Select value="userType" onChange={UserTypeChange}>
-                                <option value="default">All users</option>
-                                <option value="Students">Students</option>
-                                <option value="Parents">Parents</option>
-                                <option value="Tutors">Tutors</option>
-                            </Form.Select>
-                        </Col>
-                        <Col>
-                            <Form.Label>School type: </Form.Label>
-                            <Form.Select value="schoolType" onChange={SchoolChange}>
-                                <option value="default">Both University & K-12</option>
-                                <option value="University">University</option>
-                                <option value="K12">K-12</option>
-                            </Form.Select>
-                        </Col>
-                        <Col>
-                            <Button>Select Date Range</Button>
+                <Col md={4}>
+                    <Form.Label>Signup Methods:</Form.Label>
+                    <Select
+                        options={Signup}
+                        placeholder='Please select signup method'
+                        className='inner-select'
+                    />
+                </Col>
+                <Col md={4}>
+                    <Form.Label>Account Types:</Form.Label>
+                    <Select
+                        options={User}
+                        isMulti
+                        placeholder='Please select account type(s)'
+                        className='inner-select'
+                    />
+                </Col>
+                <Col md={4}>
+                    <Form.Label>School type: </Form.Label>
+                    <Select
+                        options={School}
+                        placeholder='Please select school type'
+                        className='inner-select'
+                    />
+                </Col>
+            </Row>
+
+            <Row>
+                <Button ref={target} onClick={() => setOpen(!isOpen)}>Select Date Range</Button>
+                <Overlay target={target.current} show={isOpen} placement="bottom">
+                    {({
+                        placement: _placement,
+                        arrowProps: _arrowProps,
+                        show: _show,
+                        popper: _popper,
+                        hasDoneInitialMeasure: _hasDoneInitialMeasure,
+                        ...props
+                    }) => (
+                        <div
+                            {...props}
+                            style={{
+                                position: 'absolute',
+                                backgroundColor: 'rgba(100, 20, 20, 1)',
+                                padding: '2px 10px',
+                                color: 'white',
+                                borderRadius: 3,
+                                ...props.style,
+                            }}
+                        >
                             <DayPicker animate mode="range"></DayPicker>
-                        </Col>
-                    </Form.Group>
-                </Form>
+                        </div>
+                    )}
+                </Overlay>
             </Row>
         </Container>
     );

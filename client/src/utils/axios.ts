4f98bc255b5b36112761902fe3e5e695e2addfea
Joshua Miller <84877484+joshakaw@users.noreply.github.com>
2025-06-05 21:26:24 -0400

    axios

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: 'http://localhost:5555/',
     headers: {
-        //  Authorization: `<Your Auth Token>`,
-        Content- Type: "application/json",
-    timeout: 1000,
-},
-  // .. other options
+        "Content-Type": "application/json",
+        "timeout": 1000,
+    }
+
+    // .. other options
 });
 
 export default instance;

96150b69f85dd365f6bb42995db49b0affa3e732
Joshua Miller <84877484+joshakaw@users.noreply.github.com>
2025-06-10 17:06:04 -0400

    Fix SignupsByCategoryRequestDTO to include accountType list on Client and Server, create Detailed Signups Request DTO (FlatDetailedSignups) on Server, and helpfully type single item lists (returned from query params parser) as SingleItemList

--- Server/Server/dtos/dtos.py
+++ Server/Server/dtos/dtos.py
@@ -1,4 +1,4 @@
-from typing import Generic, List, Literal, Optional, TypeVar, TypedDict, Union
+from typing import (    Generic,    List,    Literal,    Optional,    TypeAlias,    TypeVar,    TypedDict,    Union,)
 from pydantic import BaseModel
 
 
@@ -9,71 +9,89 @@ class RequestDTO(BaseModel):
 
     pass
 
+"""
+A list with a single item.
+"""
+SingleItemList: TypeAlias = List
 
-class DetailedSignupRequestDTO(RequestDTO):
-    """
-    Request body DTO for filtering
-    GET ---------
-    """
+# class DetailedSignupRequestDTO(RequestDTO):
+#     """
+#     Request body DTO for filtering
+#     GET ---------
+#     """
 
+#     signupMethodCategories: Union[List[str], None]
+#     freeResponseSearchKeyword: Union[str, None]
+#     startDate: Union[str, None]
+#     endDate: Union[str, None]
+#     accountType: Union[List[str], None]
+#     educationLevel: Union[List[str], None]
+
+
+# class MailchimpUsersRequestDTO(RequestDTO):
+#     """
+#     Request body DTO for filtering
+#     GET /mailchimpDashboard/users
+#     """
+
+#     studentNameSearchKeyword: Union[str, None]
+#     minNumberOfSessions: Union[int, None]
+#     maxNumberOfSessions: Union[int, None]
+#     accountType: Union[List[str], None]
+#     startDate: Union[str, None]
+#     endDate: Union[str, None]
+
+
+# class SignupsByCategoryRequestDTO(RequestDTO):
+#     """
+#     Request body DTO for filtering
+#     GET /signupDashboard/signupsByCategory
+#     """
+
+#     signupMethodCategories: Union[List[str], None]
+#     startDate: Union[str, None]
+#     endDate: Union[str, None]
+
+
+# T = TypeVar("T")
+
+
+# class ApiPaginatedRequest(BaseModel, Generic[T]):
+#     """
+#     Request interface for getting a paginated response.
+#     Filter is an object that extends RequestDTO (e.g. MailchimpUsersRequestDTO)
+#     """
+
+#     pageIndex: int
+#     pageSize: int
+#     filter: T  # Request DTO
+
+
+class FlatMailchimpPaginated(BaseModel):
+    pageIndex: SingleItemList[int]
+    pageSize: SingleItemList[int]
+    studentNameSearchKeyword: Union[SingleItemList[str], None]
+    minNumberOfSessions: Union[SingleItemList[str], None]
+    maxNumberOfSessions: Union[SingleItemList[str], None]
+    accountType: Union[List[str], None]
+    startDate: Union[SingleItemList[str], None]
+    endDate: Union[SingleItemList[str], None]
+
+
+class FlatSignups(BaseModel):
     signupMethodCategories: Union[List[str], None]
-    freeResponseSearchKeyword: Union[str, None]
-    startDate: Union[str, None]
-    endDate: Union[str, None]
+    accountType: Union[List[str], None]
+    startDate: Union[SingleItemList[str], None]
+    endDate: Union[SingleItemList[str], None]
+
+
+class FlatDetailedSignups(BaseModel):
+    signupMethodCategories: Union[List[str], None]
+    freeResponseSearchKeyword: Union[SingleItemList[str], None]
+    startDate: Union[SingleItemList[str], None]
+    endDate: Union[SingleItemList[str], None]
     accountType: Union[List[str], None]
     educationLevel: Union[List[str], None]
 
 
-class MailchimpUsersRequestDTO(RequestDTO):
-    """
-    Request body DTO for filtering
-    GET /mailchimpDashboard/users
-    """
-
-    studentNameSearchKeyword: Union[str, None]
-    minNumberOfSessions: Union[int, None]
-    maxNumberOfSessions: Union[int, None]
-    accountType: Union[List[str], None]
-    startDate: Union[str, None]
-    endDate: Union[str, None]
-
-
-class SignupsByCategoryRequestDTO(RequestDTO):
-    """
-    Request body DTO for filtering
-    GET /signupDashboard/signupsByCategory
-    """
-
-    signupMethodCategories: Union[List[str], None]
-    startDate: Union[str, None]
-    endDate: Union[str, None]
-
-
-T = TypeVar("T")
-
-
-class ApiPaginatedRequest(BaseModel, Generic[T]):
-    """
-    Request interface for getting a paginated response.
-    Filter is an object that extends RequestDTO (e.g. MailchimpUsersRequestDTO)
-    """
-
-    pageIndex: int
-    pageSize: int
-    filter: T  # Request DTO
-
-
-class FlatMailchimpPaginated(BaseModel):
-    pageIndex: List[int]
-    pageSize: List[int]
-    studentNameSearchKeyword: Union[List[str], None]
-    minNumberOfSessions: Union[List[str], None]
-    maxNumberOfSessions: Union[List[str], None]
-    accountType: Union[List[str], None]
-    startDate: Union[List[str], None]
-    endDate: Union[List[str], None]
-
-class FlatSignups(BaseModel):
-    signupMethodCategories: Union[List[str], None]
-    startDate: Union[List[str], None]
-    endDate: Union[List[str], None]
+# TODO: Markup lists that should only contain one element after query param interpretation

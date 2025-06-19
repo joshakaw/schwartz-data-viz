676e2faa36e72d7fd4e1211e67973c2c0ce47d22
coppestj <rovertjc23@gmail.com>
2025-06-18 22:17:20 -0400

    Added in the Routes for the accounts-receivable and the tutor-data pages, as well as created their pages. Changed navigation links in NavbarComponent to use regular paths for "Accounts Receivable" and "Tutor Data".

---
+++ client/src/components/pages/TutorDataDashboard/TutorDataDashboard.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './TutorDataDashboard.css';
+
+interface TutorDataDashboardProps {}
+
+const TutorDataDashboard: FC<TutorDataDashboardProps> = () => (
+  <div className="TutorDataDashboard">
+    TutorDataDashboard Component
+  </div>
+);
+
+export default TutorDataDashboard;

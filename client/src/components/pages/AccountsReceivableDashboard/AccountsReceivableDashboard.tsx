0b2ce83d3ca21229d3756405e082a315278f23a2
rehtyric <salispahict@gmail.com>
2025-07-02 21:15:12 -0400

    Made new table. can't fill with data yet, i need the dto finished for it

--- client/src/components/pages/AccountsReceivableDashboard/AccountsReceivableDashboard.tsx
+++ client/src/components/pages/AccountsReceivableDashboard/AccountsReceivableDashboard.tsx
@@ -1,12 +1,41 @@
 import React, { FC } from 'react';
 import './AccountsReceivableDashboard.css';
+import { Container, Row, Table } from 'react-bootstrap';
 
-interface AccountsReceivableDashboardProps {}
+// Will set to proper DTO when it is complete.
+interface AccountsReceivableDashboardProps {
+    data: [];
+}
 
 const AccountsReceivableDashboard: FC<AccountsReceivableDashboardProps> = () => (
-  <div className="AccountsReceivableDashboard">
-    AccountsReceivableDashboard Component
-  </div>
+    <Container>
+        <Row>
+            <h1>Accounts Receivable Dashboard</h1>
+            <p>Welcome to the Accounts Receivable Dashboard. *put description here*</p>
+        </Row>
+        <Row>
+            <Table striped bordered responsive>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>Tutor First Name</th>
+                        <th>Subject</th>
+                        <th>Date of Session</th>
+                        <th>Amt. Owed</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <td>hello</td>
+                    <td>hello</td>
+                    <td>hello</td>
+                    <td>hello</td>
+                    <td>hello</td>
+                </tbody>
+            </Table>
+        </Row>
+    </Container>
 );
 
 export default AccountsReceivableDashboard;

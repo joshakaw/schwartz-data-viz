f44983350e250be8448edbcdf27775b477456c29
rehtyric <salispahict@gmail.com>
2025-07-29 20:58:05 -0400

    Changes made to home page to fix styling and added descs to buttons

--- client/src/components/pages/RouterHomeComponent/RouterHomeComponent.tsx
+++ client/src/components/pages/RouterHomeComponent/RouterHomeComponent.tsx
@@ -40,30 +40,35 @@ const RouterHomeComponent: FC<RouterHomeComponentProps> = () => {
             </Row>
             <Row>
                 <Col md={4}>
-                    <Card onClick={(e) => handleNavigation(e, "/signups")} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '5rem' }} className="rounded">
-                        <h3 style={{ paddingLeft: '0.2rem' }}>Signups Summary</h3>
+                    <Card onClick={(e) => handleNavigation(e, "/signups")} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '1rem', cursor: 'pointer', display: 'flex', textAlign: 'center' }} className="rounded">
+                        <h3 style={{ paddingTop: '1rem' }}>Signups Summary</h3>
+                        <p>View performance of advertising types used by Schwartz Tutoring</p>
                     </Card>
                 </Col>
                 <Col md={4}>
-                    <Card onClick={(e) => handleNavigation(e, "/detailedsignups")} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '5rem' }} className="rounded">
-                        <h3 style={{ paddingLeft: '0.2rem' }}>Detailed Signups Summary</h3>
+                    <Card onClick={(e) => handleNavigation(e, "/detailedsignups")} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '1rem', cursor: 'pointer', display: 'flex', textAlign: 'center' }} className="rounded">
+                        <h3 style={{ paddingTop: '1rem' }}>Detailed Signups Summary</h3>
+                        <p>View how users of Schwartz Tutoring began using our services, and which school they're from</p>
                     </Card>
                 </Col>
                 <Col md={4}>
-                    <Card onClick={(e) => handleNavigation(e, "/mailchimp")} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '5rem' }} className="rounded">
-                        <h3 style={{ paddingLeft: '0.2rem' }}>Mailchimp</h3>
+                    <Card onClick={(e) => handleNavigation(e, "/mailchimp")} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '1rem', cursor: 'pointer', display: 'flex', textAlign: 'center' }} className="rounded">
+                        <h3 style={{ paddingTop: '1rem' }}>Mailchimp</h3>
+                        <p>View effectiveness of advertising campaigns and manage subscribers for our services</p>
                     </Card>
                 </Col>
             </Row>
             <Row style={{ paddingTop: '2rem' }}>
                 <Col md={4}>
-                    <Card onClick={(e) => handleNavigation(e, "/tutor-data")} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '5rem' }} className="rounded">
-                        <h3 style={{ paddingLeft: '0.2rem' }}>Tutor Details</h3>
+                    <Card onClick={(e) => handleNavigation(e, "/tutor-data")} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '1rem', cursor: 'pointer', display: 'flex', textAlign: 'center' }} className="rounded">
+                        <h3 style={{ paddingTop: '1rem' }}>Tutor Details</h3>
+                        <p>View performance of tutors working at Schwartz Tutoring</p>
                     </Card>
                 </Col>
                 <Col md={4}>
-                    <Card onClick={() => window.open('https://github.com/joshakaw/schwartz-data-viz', '_blank')} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '5rem' }} className="rounded">
-                        <h3 style={{ paddingLeft: '0.2rem' }}>GitHub Repository</h3>
+                    <Card onClick={() => window.open('https://github.com/joshakaw/schwartz-data-viz', '_blank')} style={{ background: 'linear-gradient(to bottom, #F5F5F5, #FFFFFF)', paddingBottom: '1rem', cursor: 'pointer', display: 'flex', textAlign: 'center' }} className="rounded">
+                        <h3 style={{ paddingTop: '1rem' }}>GitHub Repository</h3>
+                        <p>Look under the hood of the Schwartz Tutoring data analytics dashboard</p>
                     </Card>
                 </Col>
             </Row>
